73115aea600fb696234e99a8fff8c9565dd10483
mcast2185 <76968385+mcast2185@users.noreply.github.com>
2024-08-09 01:30:00 -0600

    update

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,55 +1,25 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
-
-import Header from '../../components/sectionalComponents/Header';
-import NavButtons from '../../components/animationComponents/NavButtons';
-import PageTracker from '../../components/animationComponents/PageTracker';
-import ScrollComponent from '../../components/animationComponents/ScrollComponent';
-import Home from '@/pages/home';
-import Transition from '@/pages/transition';
-import { SessionProvider, SessionProviderProps } from 'next-auth/react';
 import { Provider } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { SessionProvider, SessionProviderProps } from 'next-auth/react';
+
+import Home from '@/pages/home';
 import { store } from '../store';
 import About from '@/pages/about';
 import Gallery from '@/pages/gallery';
 import Contact from '@/pages/contact';
+import Header from '../../components/sectionalComponents/Header';
+import NavButtons from '../../components/animationComponents/NavButtons';
+import PageTracker from '../../components/animationComponents/PageTracker';
+import ScrollComponent from '../../components/animationComponents/ScrollComponent';
+
 
 
 export default function Index({session}: SessionProviderProps) {
   const SECTIONS = ['Home', 'Gallery', 'About', 'Contact'];
   const pageComponents = [ <Home/>,  <Gallery/>,  <About/>,  <Contact/>]
-  // const pageComponents = {'Home': <Home/>, 'Gallery': <Gallery/>, 'About': <About/>, 'Contact': <Contact/>}
-  const [isLoaded, setIsLoaded] = useState(false);
 
-  // const pageIndexFunction = () => {
-  //   SECTIONS.map((section: string, index: number) => {
-  //     section === 'Home' ? ()=> {
-  //     return <div key={index} id={`section-${index}`} className='section'>
-  //       <Home /> 
-  //     </div> }
-  //     : section === 'Gallery' ? ()=> {
-  //       console.log()
-  //     return <div key={index} id={`section-${index}`} className='section'>
-  //       <Gallery />
-  //     </div> }
-  //     : section === 'About' ? ()=> {
-  //       console.log()
-  //     return <div key={index} id={`section-${index}`} className='section'>
-  //       <About />
-  //     </div> }
-  //     : section === 'Contact' ? ()=> {
-  //       console.log()
-  //     return <div key={index} id={`section-${index}`} className='section'>
-  //       <Contact />
-  //     </div> }
-  //     : console.log("User/Page: Conditional statement failed!");
-  //   })
-  // };
-
-  useEffect(() => {
-    setIsLoaded(false);
-  })
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -57,11 +27,16 @@ export default function Index({session}: SessionProviderProps) {
         <SessionProvider session={session}>
 
           <Provider store={store}>
-          {/* <Transition/> */}
             <Header />
             <ScrollComponent>
+              {/* 
+
+              grab the element from the source page and adjust the code to 
+              cohesively work with our components.
+
               <PageTracker sections={SECTIONS} />
-              <NavButtons sections={SECTIONS} />
+              <NavButtons sections={SECTIONS} /> 
+              */}
               {pageComponents.map((page, index) => (
                 <div
                   key={index}
@@ -71,7 +46,6 @@ export default function Index({session}: SessionProviderProps) {
                   {page}
                 </div>
               ))}
-
             </ScrollComponent>
           </Provider>
         </SessionProvider>
--- src/pages/transition.tsx
+++ src/pages/transition.tsx
@@ -4,6 +4,7 @@ import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import React, { useEffect, useState } from 'react';
 
+import Home from './home';
 
 // import Header from '../components/sectionalComponents/Header';
 
@@ -11,85 +12,10 @@ const Transition = () => {
   gsap.registerPlugin(useGSAP);
   const [isFirstLoad, setIsFirstLoad] = useState(false);
   const [isLoaded, setIsLoaded] = useState(false);
-  const {contextSafe} = useGSAP(() => {});
 
-  const DynamicElement = () => {
-    contextSafe(()=> {
-      gsap.from(".split-text", {
-        clipPath: "inset(0 0 50% 0)",
-        ease: "power3.in",
-        transform: "translateY(0%)",
-        opacity: 1,
-        duration: 1.5
-      });
+  const pageAnimation = () => {
+    // setIsLoaded(true);
 
-      gsap.to(".split-text", {
-        clipPath: "inset(0 0 52% 0)",
-        ease: "power4.inOut",
-        transform: "translateX(100vw) translateY(0%)",
-        delay: 1.5,
-        opacity: 0,
-        transitionProperty: "transform, opacity",
-        transitionDelay: 1.5,
-        transitionDuration: 3
-      });
-
-      gsap.from(".split-text-two", {
-        clipPath: "inset(50% 0 0 0)",
-        ease: "power3.in",
-        transform: "translateY(0%)",
-        opacity: 1,
-        duration: 1.5
-      });
-
-      gsap.to(".split-text-two", {
-        clipPath: "inset(52% 0 0 0)",
-        ease: "power4.inOut",
-        transform: "translateX(-100vw) translateY(0%)",
-        delay: 1.5,
-        opacity: 0,
-        transitionProperty: "transform, opacity",
-        transitionDelay: 1.5,
-        transitionDuration: 3
-      });
-    });
-
-    console.log(contextSafe)
-
-    // return (
-    //   <div className='split-text-wrapper relative w-full h-full flex justify-center items-center'>
-    //     <h1 className='absolute will-change-transform split-text text-[16vw] h-[62px] text-blue-600 overflow-hidden'>
-    //       Anomar
-    //     </h1>
-    //     <h1 className='absolute will-change-transform split-text-two text-[16vw] h-[62px] text-red-600 overflow-hidden'>
-    //       Anomar
-    //     </h1>
-
-    //   </div>
-    // );
-  };
-
-
-  useEffect(() => {
-    console.log("useEffect fired first", contextSafe);
-    // Check if it's the first load
-    const isFirstLoad = localStorage.getItem('isFirstLoad');
-
-    if (!isFirstLoad) {
-      // Set flag to indicate the homepage has been loaded once
-      localStorage.setItem('isFirstLoad', 'true');
-      setIsFirstLoad(true);
-    }
-
-    if (isFirstLoad === null) {
-      // Run the GSAP animation only if it’s the first load
-      gsap.fromTo('.animate-on-load', { opacity: 0 }, { opacity: 1, duration: 2, zIndex: -50 });
-    }
-  }, []);
-
-
-  useGSAP(() => {
-    console.log("useGSAP fired first", contextSafe);
     gsap.from(".clip-top, .clip-bottom", 2, {
       delay: 1,
       height: "50vh",
@@ -134,122 +60,153 @@ const Transition = () => {
     });
 
     gsap.to(".clip-top .marquee, .clip-bottom .marquee, .clip-center .marquee span, .marquee p", 1, {
-      callbackScope: ()=> setIsLoaded(true),
       delay: 6.5,
       // opacity: 0,
       zIndex: -10,
       ease: "power2.inOut",
     });
 
-    gsap.to(".header-element", 1, {
-      delay: 6.5,
+    gsap.to(".loader", 3.75, {
+      delay: 7.5,
+      background: "#0f0f10",
+      ease: "power2.inOut",
+    });
+
+    gsap.to(".loader", 1.5, {
+      delay: 8.25,
       opacity: 0,
       zIndex: -10,
       ease: "power2.inOut",
     });
+  };
 
-  })
 
+
+
+  useEffect(() => {
+    // Check if it's the first load
+    const isFirstLoad = localStorage.getItem('isFirstLoad');
+
+    if (!isFirstLoad) {
+      // Set flag to indicate the homepage has been loaded once
+      localStorage.setItem('isFirstLoad', 'true');
+      setIsFirstLoad(true);
+    }
+
+    if (isFirstLoad === null) {
+      // Run the GSAP animation only if it’s the first load
+      gsap.fromTo('.animate-on-load', { opacity: 0 }, { opacity: 1, duration: 2, zIndex: -50 });
+    }
+  }, [setIsFirstLoad]);
+
+
+  useGSAP(() => {
+
+    setIsLoaded(true);
+    console.log("#2", isLoaded);
+    pageAnimation();
+    
+    if (isLoaded === true) {
+      isLoaded
+    }
+  }, [pageAnimation]);
+
+  // isLoaded === false && setIsLoaded(true);
+
+  console.log(isLoaded)
+  
   return (
-    <div className=".animate-on-load">
-      <div className="loader">
-        <div className="loader-clip clip-top">
-          <div className="marquee">
-            <div className="marquee-container font-Megrim">
-              <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
-                Anomar 
-              </span> 
-              
-              <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
-                Anomar
-              </span> 
-              
+    <div className='page-transition-wrapper '>
+     
+      <div className=".animate-on-load">
+        <div className="loader">
+          <div className="loader-clip clip-top">
+            <div className="marquee">
+              <div className="marquee-container font-Megrim">
+                <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
+                  Anomar 
+                </span> 
+                
+                <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
+                  Anomar
+                </span> 
+                
 
-              <h3 className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
-                Anomar
-              </h3>
-              
-              
-              <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
-                Anomar
-              </span> 
-              
-              <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
-                Anomar
-              </span> 
-              
-            </div>
-          </div>
-        </div>
-        <div className="loader-clip clip-bottom">
-          <div className="marquee">
-            <div className="marquee-container font-Megrim">
-              <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
-                Anomar
-              </span> 
-              
-              <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
-                Anomar
-              </span> 
-              
-              
-              <h3 className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
-                Anomar
-              </h3>
-              
-
-              <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
-                Anomar
-              </span> 
-              
-              <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
-                Anomar
-              </span> 
-              
-            </div>
-          </div>
-        </div>
-        <div className="clip-center">
-          <div className="marquee">
-            <div className="marquee-container font-Megrim">
-              <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
-                Anomar
-              </span> 
-              
-              {isLoaded === false && (
-                <h3 className='header-element text-[16vw] px-12 pr-[135px] sm:pr-8'>
+                <h3 className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
                   Anomar
                 </h3>
-                )} : {( 
-                <div className='split-text-wrapper relative w-full h-full flex justify-center items-center'>
-                  <h1 className='absolute will-change-transform split-text text-[16vw] h-[62px] text-blue-600 overflow-hidden'>
-                    Anomar
-                  </h1>
-                  <h1 className='absolute will-change-transform split-text-two text-[16vw] h-[62px] text-red-600 overflow-hidden'>
-                    Anomar
-                  </h1>
-                </div>
-              )}
- 
-              <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
-                Anomar
-              </span> 
-              
+                
+                
+                <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
+                  Anomar
+                </span> 
+                
+                <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
+                  Anomar
+                </span> 
+              </div>
+            </div>
+          </div>
+          <div className="loader-clip clip-bottom">
+            <div className="marquee">
+              <div className="marquee-container font-Megrim">
+                <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
+                  Anomar
+                </span> 
+                
+                <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
+                  Anomar
+                </span> 
+                
+                
+                <h3 className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
+                  Anomar
+                </h3>
+                
 
-              <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
-                Anomar
-              </span> 
-              
-              <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
-                Anomar
-              </span> 
-              
+                <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
+                  Anomar
+                </span> 
+                
+                <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
+                  Anomar
+                </span> 
+              </div>
+            </div>
+          </div>
+          <div className="clip-center">
+            <div className="marquee">
+              <div className="marquee-container font-Megrim">
+                <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
+                  Anomar
+                </span> 
+                
+
+                <h3 className='main-header-element text-[16vw] px-12 pr-[135px] sm:pr-8'>
+                  Anomar
+                </h3>
+                  
+                  
+                <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
+                  Anomar
+                </span> 
+                
+
+                <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
+                  Anomar
+                </span> 
+                
+                <span className='text-[16vw] px-12 pr-[135px] sm:pr-8'>
+                  Anomar
+                </span> 
+              </div>
             </div>
           </div>
         </div>
       </div>
-      {/* <Header/> */}
-      
+      <div className=''>
+        {!isLoaded && <Home props={isLoaded} />}
+      </div>
     </div>
   );
 };
